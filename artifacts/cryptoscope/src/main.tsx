41a9e43ad37bed3777e1373e9a3a018be833f9f6
Lildevil101 <kiddem44@gmail.com>
2026-03-19 07:18:37 +0100

    Unregister service workers before registering new one

--- artifacts/cryptoscope/src/main.tsx
+++ artifacts/cryptoscope/src/main.tsx
@@ -3,6 +3,12 @@ import App from "./App";
 import "./index.css";
 import { registerSW } from 'virtual:pwa-register';
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then((regs) => {
+    regs.forEach((reg) => reg.unregister());
+  });
+}
+
 registerSW({ immediate: true });
 
 createRoot(document.getElementById("root")!).render(<App />);
